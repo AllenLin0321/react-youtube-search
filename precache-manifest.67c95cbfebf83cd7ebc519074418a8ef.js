d89ee31beb906a96817c9ae1f1441051b8481dee
Allen <glux7381@gmail.com>
2019-10-19 22:34:43 +0800

    Updates

---
+++ precache-manifest.67c95cbfebf83cd7ebc519074418a8ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8429cfe81018dea895afa78c33c8660f",
+    "url": "/react-youtube-search/index.html"
+  },
+  {
+    "revision": "12951542210711389ad8",
+    "url": "/react-youtube-search/static/css/main.a89220aa.chunk.css"
+  },
+  {
+    "revision": "2a18000df74d0c2f26b9",
+    "url": "/react-youtube-search/static/js/2.0f71e33c.chunk.js"
+  },
+  {
+    "revision": "12951542210711389ad8",
+    "url": "/react-youtube-search/static/js/main.6b65e91e.chunk.js"
+  },
+  {
+    "revision": "0ed043947d2f4cf28a4d",
+    "url": "/react-youtube-search/static/js/runtime-main.727cc70d.js"
+  }
+]);
